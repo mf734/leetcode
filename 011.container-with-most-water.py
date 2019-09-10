d9e3c56ea3ba52e32b86b4c38ec366c7d24474e8
Manquan Fang <42682039+mf734@users.noreply.github.com>
2019-09-10 01:18:06 -0400

    Add files via upload

--- 011.container-with-most-water.py
+++ 011.container-with-most-water.py
@@ -1,11 +1,7 @@
 class Solution(object):
 	def maxArea(self, height):
-		ans = left = 0
-		right = len(height) - 1
-		while left < right:
-			ans = max(ans, (right - left) * min(height[left], height[right]))
-			if height[left] <= height[right]:
-				left += 1
-			else:
-				right -= 1
-		return ans
+		res, l, r = 0, 0, len(height) - 1
+		while l < r:
+			h = min(height[l], height[r])
+			res, l, r = max(res,  h * (r - l)), l + (height[l] == h), r - (height[r] == h)
+		return res
