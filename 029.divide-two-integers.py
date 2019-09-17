8df9a10a0287bf49399fe3a5d2316489c51df408
Manquan Fang <42682039+mf734@users.noreply.github.com>
2019-09-17 09:54:11 -0400

    Add files via upload

---
+++ 029.divide-two-integers.py
@@ -0,0 +1,16 @@
+class Solution(object):
+	def divide(self, dividend, divisor):
+		sign = (dividend > 0) ^ (divisor > 0)
+		# sign = (dividend < 0) == (divisor < 0)
+		dividend = abs(dividend)
+		divisor = abs(divisor)
+		result = 0
+		# 一直让divisor大于dividend
+		while dividend >= divisor:
+			x = 0
+			# divisor*(2^x) is just >= dividend
+			while dividend >= divisor << (x+1):
+				x += 1
+			result += 1 << x
+			dividend -= divisor << x
+		return min(result if sign else -result, 2147483647)
