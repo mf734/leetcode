5fd40f40e2d685f1ac3a74bfa0c9d88267c1b4ec
Manquan Fang <42682039+mf734@users.noreply.github.com>
2019-09-21 01:04:24 -0400

    Add files via upload

--- 009.palindrome-number.py
+++ 009.palindrome-number.py
@@ -8,8 +8,14 @@ class Solution(object):
 		while x > y:
 			y = y*10 + x%10
 			x = x//10
-		return y == x or (y > x and y//10 == x)
-	
-	# Python way
-	def isPalindrom2(self, x):
-		return False if x < 0 else x == int(str(x)[::-1])
+		# return y == x or (y > x and y//10 == x)
+		return x == y or x == y//10
+
+	# map, lambda
+    def isPalindrome(self, x: int) -> bool:
+        r = list(map(lambda i: int(10**-i * x % 10), range(int(math.log10(x)), -1, -1))) if x > 0 else [0, x]
+        return r == r[::-1]
+
+	# Pathonic
+	def isPalindrom3(self, x):
+		return str(x) == str(x)[::-1]
